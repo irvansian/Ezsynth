0ccc71ad881527b48b2aa2f07860040444ebacc1
Irvan Sian Syah Putra <71798164+irvansian@users.noreply.github.com>
2023-10-20 03:43:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ezsynthfork',
+    name='ezsynth',
     version='1.2.1.1',
     packages=find_packages(),
     include_package_data=True,
